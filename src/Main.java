e0b01dab0e0c0fe436088ce69612478b632e0684
mohamekni <mekni.moha99@gmail.com>
2025-09-24 12:16:10 +0200

    Learn Java

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // This Is My First Java Program
+
+        /*
+        This
+        Is Multi-line
+        Comment
+         */
+
+        System.out.print("I Like Pizza !\n");
+        System.out.print("It's Really Good\n");
+        System.out.print("Buy Me Pizza !");
+        System.out.println("\n\n");
+    }
+}
